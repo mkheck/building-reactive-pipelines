639c20d16d28de9f960871c6d808f5a634f007b0
Mark Heckler <mark.heckler@gmail.com>
2019-02-22 16:05:07 +0200

    Import cleanup

--- brp-src/src/main/java/com/thehecklers/brpsrc/BrpSrcApplication.java
+++ brp-src/src/main/java/com/thehecklers/brpsrc/BrpSrcApplication.java
@@ -1,20 +1,7 @@
 package com.thehecklers.brpsrc;
 
-import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.cloud.stream.reactive.StreamEmitter;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-
-import javax.annotation.PostConstruct;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Random;
-import java.util.UUID;
 
 @SpringBootApplication
 public class BrpSrcApplication {
